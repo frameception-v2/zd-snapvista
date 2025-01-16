b0bda920520d8c51f9728480816887b616ad754d
hellno <686075+hellno@users.noreply.github.com>
2025-01-16 17:09:28 +0000

    Initial bulk copy from template

---
+++ README.md
@@ -0,0 +1,11 @@
+# farcaster frames template
+
+This repo contains a template for Farcaster v2 frames:
+✅ shadcn and tailwindcss
+
+Architecture
+
+The application is structured around the following key layers:
+	•	Providers Layer: Manages global state and configurations, including wallet connections and SDK setup.
+	•	Components Layer: Encapsulates UI elements and interactive features, such as buttons for actions and context displays.
+	•	Hooks and Utilities: Facilitates interactions with the Frame SDK and Wagmi, handling asynchronous operations and state management.
---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,24 @@
+import { ImageResponse } from "next/og";
+import { PROJECT_TITLE, PROJECT_DESCRIPTION } from "~/lib/constants";
+
+export const alt = "Farcaster Frames V2 Demo";
+export const size = {
+  width: 600,
+  height: 400,
+};
+
+export const contentType = "image/png";
+
+export default async function Image() {
+  return new ImageResponse(
+    (
+      <div tw="h-full w-full flex flex-col justify-center items-center relative bg-white">
+        <h1 tw="text-6xl">{PROJECT_TITLE}</h1>
+        <h3 tw="text-2xl">{PROJECT_DESCRIPTION}</h3>
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
